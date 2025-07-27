24687c92fc95c371e98c4b9f015a0b5da006ef4f
Ashley <afletcher679@gmail.com>
2025-07-27 01:05:56 -0400

    Add reset button

--- React-Peek-A-Boo/src/App.jsx
+++ React-Peek-A-Boo/src/App.jsx
@@ -1,13 +1,16 @@
 //This is the root React component, where the actual UI starts & where wwe build out the component tree
 import { useState } from 'react'
 import './App.css'
-
+import resetIcon from './assets/reset.png';
 function App() {
 
   return (
     <>
         <div className={"flex-container"}>
             <button className={"peek-a-boo-button"}>Peek-A-Boo!</button>
+        <button className={"reset-button"}>
+            <img src={resetIcon} alt="Reset Icon" className={"reset-icon"} />
+        </button>
         </div>
     </>
   )
