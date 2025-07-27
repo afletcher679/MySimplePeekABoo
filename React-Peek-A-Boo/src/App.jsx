83292ad22eb9f487c9b372fcef936771534d6dfa
Ashley <afletcher679@gmail.com>
2025-07-27 01:53:45 -0400

    add leaf left and right. adjust some css

--- React-Peek-A-Boo/src/App.jsx
+++ React-Peek-A-Boo/src/App.jsx
@@ -1,13 +1,19 @@
 //This is the root React component, where the actual UI starts & where wwe build out the component tree
 import { useState } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import resetIcon from './assets/reset.png';
+import leafCluster from './assets/leaves/monstera_leaves_cluster.png';
 function App() {
 
   return (
     <>
-        <div className={"flex-container"}>
-            <button className={"peek-a-boo-button"}>Peek-A-Boo!</button>
+        <div className={"left-right-leaf-clusters-container"}>
+            <img src={leafCluster} alt="Leaf Clusters" className={"left-right-leaf-clusters left"}/>
+            <img src={leafCluster} alt="Leaf Clusters" className={"left-right-leaf-clusters"}/>
+        </div>
+        <div className={"pt-1 flex-container"}>
+            <button className={"peek-a-boo-button p-1"}>Peek-A-Boo!</button>
         <button className={"reset-button"}>
             <img src={resetIcon} alt="Reset Icon" className={"reset-icon"} />
         </button>
