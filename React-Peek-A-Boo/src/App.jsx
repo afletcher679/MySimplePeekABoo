0570cacd76bddd8ce3110c444276c81b58769dbf
Ashley <afletcher679@gmail.com>
2025-07-27 00:10:38 -0400

    Add comments for structure clarity

--- React-Peek-A-Boo/src/App.jsx
+++ React-Peek-A-Boo/src/App.jsx
@@ -1,3 +1,4 @@
+//This is the root React component, where the actual UI starts & where wwe build out the component tree
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
--- React-Peek-A-Boo/src/main.jsx
+++ React-Peek-A-Boo/src/main.jsx
@@ -1,3 +1,5 @@
+// This the entry point for the React app. It mounts the React app to the DOM.
+// It links to App.jsx and applies global styles from index.css. 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
